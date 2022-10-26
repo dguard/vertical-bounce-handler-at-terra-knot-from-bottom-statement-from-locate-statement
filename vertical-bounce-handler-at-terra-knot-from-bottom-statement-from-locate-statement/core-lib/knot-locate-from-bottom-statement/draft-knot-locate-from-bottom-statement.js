32108cf58eb10ffa40a514a28eb0fa3cb8a2edd1
Alexander Serditov <keep@digitallyconstructed.ru>
2022-10-26 23:17:35 +0500

    initial commit

--- vertical-bounce-handler-at-terra-knot-from-bottom-statement-from-locate-statement/core-lib/knot-locate-from-bottom-statement/draft-knot-locate-from-bottom-statement.js
+++ vertical-bounce-handler-at-terra-knot-from-bottom-statement-from-locate-statement/core-lib/knot-locate-from-bottom-statement/draft-knot-locate-from-bottom-statement.js
@@ -4,40 +4,112 @@
 function Traversar() {
     let that = this
 
-    that.traverseFromBottomStatementAtTopLeft = function (pointerBelongings, connectedKnot) {
+    let connectedKnot
 
+    that.traverseFromBottomStatementAtTopLeft = function (pointerBelongings, connectedKnotInstance) {
+        // keep
+        let traversar = that
+
+        // keep !
+        connectedKnot = connectedKnotInstance
+
+        that.pointerBelongingsAtTopLeft = pointerBelongings
+        // keep !
     }
-    that.traverseFromBottomStatementAtBottomLeft = function () {
-
+    that.traverseFromBottomStatementAtBottomLeft = function (pointerBelongings, connectedKnotInstance) {
+        that.pointerBelongingsAtBottomLeft = pointerBelongings
     }
-    that.traverseFromBottomStatementAtBottomRight = function () {
-
+    that.traverseFromBottomStatementAtBottomRight = function (pointerBelongings, connectedKnotInstance) {
+        that.pointerBelongingsAtBottomRight = pointerBelongings
     }
-    that.traverseFromBottomStatementAtTopRight = function () {
-
+    that.traverseFromBottomStatementAtTopRight = function (pointerBelongings, connectedKnotInstance) {
+        // keep !
+        that.pointerBelongingsAtTopRight = pointerBelongings
     }
 
-    that.useKnotRelativeSize = function () {
-
+    let pointerBelongingsAtKnotRelativeSize
+    that.usePointerBelongingsAtKnotRelativeSize = function (pointerBelongingsAtKnotRelativeSizeInstance) {
+        pointerBelongingsAtKnotRelativeSize = pointerBelongingsAtKnotRelativeSizeInstance
     }
+
+    let knotRelativeSize
+
+    that.passThrough = function (connectedKnotInstance, fz) {
+        let traversar = that
+
+        fz()
+
+        return traversar
+    }
+
+    that.passThroughRelativeSize = function (knotRelativeSizeInstance) {
+        let traversar = that
+
+        return traversar
+    }
+
+    that.useKnotRelativeSize = function (knotRelativeSizeInstance) {
+        knotRelativeSize = knotRelativeSizeInstance
+    }
+    // keep !
+    // traversarIN ! \g // soul \ mine
     that.startTraversaring = function () {
         // connectedKnot
         let traversar = that
 
-        // event on knot relative size begin
-        // connectedKnot
-        // .captureFromBottomStatementAtTopLeft(pointerBelongingsAtTopLeft)
-        // .captureFromBottomStatementAtBottomLeft(pointerBelongingsAtBottomLeft)
-        // .captureFromBottomStatementAtBottomRight(pointerBelongingsAtBottomRight)
-        // .captureFromBottomStatementAtRightTop(pointerBelongingsAtTopRight)
-        // .useRelativeSize(knotRelativeSize, traversar)
-        // .takeCapture()
+        that.passThroughRelativeSize(knotRelativeSize).passThrough(connectedKnot, function () {
+            // event on knot relative size begin
 
-        // event on knot relative size completed
+            pointerBelongingsAtKnotRelativeSize.throughCtx(/* keep */).throughInteraction(function(ctx) {
+
+                connectedKnot
+                .useCtx(ctx)
+                .captureFromBottomStatementAtTopLeft(pointerBelongingsAtTopLeft)
+                .captureFromBottomStatementAtBottomLeft(pointerBelongingsAtBottomLeft)
+                .captureFromBottomStatementAtBottomRight(pointerBelongingsAtBottomRight)
+                .captureFromBottomStatementAtRightTop(pointerBelongingsAtTopRight)
+                .throughCtx(ctx)
+                .useRelativeSize(knotRelativeSize, traversar)
+                .takeCapture()
+
+                // that \ spars tree ! \m
+            })
+            // event on knot relative size completed
+        })
     }
 }
 
-function echoLocatorAtKnotLocate() {
+function PointerBelongingsAtKnotRelativeSize(knotRelativeSize) {
+    let that = this
+
+    let ctx
+
+    that.throughInteraction = function (fy) {
+        let pointerBelongings = that
+
+        fy(ctx)
+
+        return pointerBelongings
+    }
+
+    that.throughCtx = function () {
+        let pointerBelongings = that
+        ctx = knotRelativeSize.connectedToCanvas.ctx
+
+        return pointerBelongings
+    }
+}
+
+function KnotLocateFromBottomStatement() {
+    let that = this
+    // keep
+
+    that.compute = function () {
+
+    }
+}
+
+function EchoLocatorAtKnotLocate() {
     let that = this
 
     // knotFromBottomStatementAtTopLeft
@@ -51,13 +123,20 @@ function echoLocatorAtKnotLocate() {
     // knotFromBottomStatement
     //
     // use knotLocate
+
     let knotRelativeSize
     that.useRelativeSize = function (knot) {
         knotRelativeSize = knot
     }
 
+    let pointerBelongings
+    that.usePointerBelongings = function (pointerBelongingsInstance) {
+        pointerBelongings = pointerBelongingsInstance
+    }
+
     that.traverseInteractionToKnotAtProfile = function (knotConnectedToProfile) {
         // use knotRelativeSize
+        let knot = knotConnectedToProfile
 
         (new Traversar())
             .traverseFromBottomStatementAtTopLeft(knot.throughPointerBelongingsAtTopLeft, knotConnectedToProfile.throughConnectedKnot)
@@ -65,26 +144,33 @@ function echoLocatorAtKnotLocate() {
             .traverseFromBottomStatementAtBottomRight(knot.throughPointerBelongingsAtBottomRight, knotConnectedToProfile.throughConnectedKnot)
             .traverseFromBottomStatementAtTopRight(knot.throughPointerBelongingsAtTopRight, knotConnectedToProfile.throughConnectedKnot)
             .useKnotRelativeSize(knotRelativeSize)
+            .throughPointerBelongingsAtKnotRelativeSize(pointerBelongings)
             .startTraversaring()
     }
 
 
     that.traverseInteractionToKnotAtFront = function (knotConnectedToFront) {
+        let knot = knotConnectedToFront
+
         (new Traversar())
             .traverseFromBottomStatementAtTopLeft(knot.throughPointerBelongingsAtTopLeft, knotConnectedToFront.throughConnectedKnot)
             .traverseFromBottomStatementAtBottomLeft(knot.throughPointerBelongingsAtBottomLeft, knotConnectedToFront.throughConnectedKnot)
             .traverseFromBottomStatementAtBottomRight(knot.throughPointerBelongingsAtBottomRight, knotConnectedToFront.throughConnectedKnot)
             .traverseFromBottomStatementAtTopRight(knot.throughPointerBelongingsAtTopRight, knotConnectedToFront.throughConnectedKnot)
             .useKnotRelativeSize(knotRelativeSize)
+            .throughPointerBelongingsAtKnotRelativeSize(pointerBelongings)
             .startTraversaring()
     }
     that.traverseInteractionToKnotFromBottomStatement = function (knotConnectedToBottomStatement) {
+        let knot = knotConnectedToBottomStatement
+
         (new Traversar())
             .traverseFromBottomStatementAtTopLeft(knot.throughPointerBelongingsAtTopLeft, knotConnectedToBottomStatement.throughConnectedKnot)
             .traverseFromBottomStatementAtBottomLeft(knot.throughPointerBelongingsAtBottomLeft, knotConnectedToBottomStatement.throughConnectedKnot)
             .traverseFromBottomStatementAtBottomRight(knot.throughPointerBelongingsAtBottomRight, knotConnectedToBottomStatement.throughConnectedKnot)
             .traverseFromBottomStatementAtTopRight(knot.throughPointerBelongingsAtTopRight, knotConnectedToBottomStatement.throughConnectedKnot)
             .useKnotRelativeSize(knotRelativeSize)
+            .throughPointerBelongingsAtKnotRelativeSize(pointerBelongings)
             .startTraversaring()
     }
 
@@ -120,35 +206,73 @@ function KnotFromBottomStatement(pencil) {
 
     pencil.rememberSample(that.provideKnot())
 
+    that.highlightPointerBelongings = function (highlightPointerBelongingsAtTopLeft, highlightPointerBelongingsAtBottomLeft, highlightPointerBelongingsAtBottomRight, highlightPointerBelongingsAtRightTop) {
+        that.pointerBelongingsAtTopLeft = highlightPointerBelongingsAtTopLeft
+        that.pointerBelongingsAtBottomLeft = highlightPointerBelongingsAtBottomLeft
+        that.pointerBelongingsAtBottomRight = highlightPointerBelongingsAtBottomRight
+        that.pointerBelongingsAtRightTop = highlightPointerBelongingsAtRightTop
+    }
+
+    that.throughCtx = function (ctx) {
+        let knotFromBottomStatement = that
+
+        knotFromBottomStatement.ctx.addEventListener('click', function (event) {
+            let providedY = event.clientY
+            let providedX = event.clientX
+
+            // traversar sends straight request
+            // and receives flipped request
+
+            if(that.pointerBelongingsAtTopLeft.y >= providedY && that.pointerBelongingsAtTopLeft.x >= providedX
+                && that.pointerBelongingsAtBottomLeft.y >= providedY && that.pointerBelongingsAtBottomLeft.x >= providedX
+                && that.pointerBelongingsAtBottomRight.y <= providedY && that.pointerBelongingsAtBottomRight.x <= providedX
+                && that.pointerBelongingsAtRightTop.y <= providedY && that.pointerBelongingsAtRightTop.x <= providedX
+            ) {
+                pencil.draw()
+            }
+        })
+    }
+
+    that.useCtx = function (ctx) {
+        that.ctx = ctx
+    }
+
     that.takeCapture = function () {
+        let knotFromBottomStatement = that
         // keep
 
         // use knot relative size
         // use pointer belongings
 
         pencil.draw()
+
+        return knotFromBottomStatement
     }
     that.provideKnot = function () {
         return that
     }
 
-    that.captureFromBottomStatementAtTopLeft = function () {
-
+    that.captureFromBottomStatementAtTopLeft = function (pointerBelongingsAtTopLeft) {
+        that.pointerBelongingsAtTopLeft = pointerBelongingsAtTopLeft
     }
-    that.captureFromBottomStatementAtBottomLeft = function () {
-
+    that.captureFromBottomStatementAtBottomLeft = function (pointerBelongingsAtBottomLeft) {
+        that.pointerBelongingsAtBottomLeft = pointerBelongingsAtBottomLeft
     }
-    that.captureFromBottomStatementAtBottomRight = function () {
-
+    that.captureFromBottomStatementAtBottomRight = function (pointerBelongingsAtBottomRight) {
+        that.pointerBelongingsAtBottomRight = pointerBelongingsAtBottomRight
     }
-    that.captureFromBottomStatementAtRightTop = function () {
-
+    that.captureFromBottomStatementAtRightTop = function (pointerBelongingsAtRightTop) {
+        that.pointerBelongingsAtRightTop = pointerBelongingsAtRightTop
     }
 
     that.useRelativeSize = function (knotRelativeSize, traversar) {
+        let knotFromBottomStatement = that
         // keep !
+
         // use knot relative size
         // use pointer belongings
+
+        return knotFromBottomStatement
     }
 
     that.useConnectedKnot = function (connectedKnot) {
@@ -156,20 +280,20 @@ function KnotFromBottomStatement(pencil) {
     }
 }
 
-function knotLocate() {
+function KnotLocate() {
     let that = this
 
     that.assignCoordinates = function () {
         // keep
     }
 
-    that.constructPointerBelongings = function () {
+    that.constructPointerBelongings = function (pointerBelongingsCollectionOfFour) {
         let that = this
+        that.throughPointerBelongingsAtTopLeft = pointerBelongingsCollectionOfFour.pointerBelongingsAtTopLeft
+        that.throughPointerBelongingsAtBottomLeft = pointerBelongingsCollectionOfFour.pointerBelongingsAtBottomLeft
 
-        // throughPointerBelongingsAtTopLeft
-        // throughPointerBelongingsAtBottomLeft
-        // throughPointerBelongingsAtBottomRight
-        // throughPointerBelongingsAtTopRight
+        that.throughPointerBelongingsAtBottomRight = pointerBelongingsCollectionOfFour.pointerBelongingsAtBottomRight
+        that.throughPointerBelongingsAtTopRight = pointerBelongingsCollectionOfFour.pointerBelongingsAtTopRight
     }
 
     that.assignKnotToKnotAtProfileStatement = function (knotAtProfileStatement) {
@@ -190,6 +314,40 @@ function Pencil() {
 
     // keep
     let knot
+
+    that.passThroughCrowd = function () {
+
+    }
+    that.joinLoop = function () {
+
+    }
+    that.compact = function () {
+
+    }
+
+    let knotThrough
+    that.intoPointerBelongingsCollection = function () {
+        let pointerBelongingsCollectionOfFour = {
+            pointerBelongingsAtTopLeft: knotThrough.pointerBelongingsAtTopLeft
+            pointerBelongingsAtBottomLeft: knotThrough.pointerBelongingsAtBottomLeft
+            pointerBelongingsAtRightBottom: knotThrough.pointerBelongingsAtRightBottom
+            pointerBelongingsAtRightTop: knotThrough.pointerBelongingsAtRightTop
+        }
+
+        // keep
+
+        return pointerBelongingsCollectionOfFour
+    }
+    that.drawThrough = function (knotThroughInstance) {
+        knotThrough = knotThroughInstance
+
+        // keep
+        // knotThroughInstance.spawnedPointerBelongings
+        knotThrough.compute().constructPointerBelongingsFromBottom()
+        // take computed pointerBelongings topLeft, bottomLeft, bottomRight, rightTop
+
+        // keep
+    }
 
     that.useKnot = function (knotInstance) {
         knot = knotInstance
